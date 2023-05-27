aaebd8bcd42d4e8906c932d7ebc85eb875336ba6
유민호 <wingedcompany@gmail.com>
2023-05-28 01:23:18 +0900

    차분히 하나씩

--- nuxt3-exam/api/blogApi.ts
+++ nuxt3-exam/api/blogApi.ts
@@ -4,13 +4,9 @@ export default {
   async findAll(props: { tag?: Ref<string>; page?: Ref<number> } = {}) {
     const config = useRuntimeConfig();
     const { tag, page } = props;
-    try {
-      return await useFetch<Link[]>("link", {
-        baseURL: config.public.apiBase,
-        params: { tag, page },
-      });
-    } catch (err) {
-      throw new Error("");
-    }
+    return await useFetch<Link[]>("link", {
+      baseURL: config.public.apiBase,
+      params: { tag, page },
+    });
   },
 };
