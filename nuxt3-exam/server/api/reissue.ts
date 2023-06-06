2f61d41e48d70f3c5ab28d4fa8b94198f516bf52
유민호 <wingedcompany@gmail.com>
2023-06-06 20:33:53 +0900

    이제 갱신할 수 있겠다

--- nuxt3-exam/server/api/reissue.ts
+++ nuxt3-exam/server/api/reissue.ts
@@ -1,8 +1,5 @@
-import { defineEventHandler, H3Event, parseCookies, setCookie } from "h3";
-
 export default defineEventHandler(async (event) => {
   const currentRtk = getCookie(event, "refresh-token");
-  console.log("@@@@@@@@@@@@@ currentRtk", { currentRtk });
   const config = useRuntimeConfig();
   const { statusCode, atk, rtk } = await $fetch<{
     statusCode: number;
@@ -12,20 +9,6 @@ export default defineEventHandler(async (event) => {
     baseURL: config.public.apiBase,
     headers: { Authorization: `Bearer ${currentRtk}` },
   });
-  console.log("@@@@@@@@@@@@@", { statusCode, atk, rtk, event });
-  setCookie(event, "123", "456");
-  setCookie(event, "1231", "4561");
-
-  if (statusCode === 401 || rtk === "") {
-    setCookie(event, "refresh-token", "", {
-      path: "/",
-      httpOnly: true,
-    });
-    return { atk: "" };
-  }
-  setCookie(event, "refresh-token", rtk, {
-    path: "/",
-    httpOnly: true,
-  });
+  if (statusCode === 401 || rtk === "") return { atk: "", rtk: "" };
   return { atk, rtk };
 });
