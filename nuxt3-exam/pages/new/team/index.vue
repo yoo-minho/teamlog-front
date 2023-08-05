0317cc5f55ec3a380632a6833b6e859764e13db2
유민호 <wingedcompany@gmail.com>
2023-08-05 19:43:27 +0900

    정릐22222

--- nuxt3-exam/pages/new/team/index.vue
+++ nuxt3-exam/pages/new/team/index.vue
@@ -3,8 +3,6 @@ import GroupApi from "@/api/groupApi";
 import TeamForm from "@/components/Form/TeamForm.vue";
 import { TeamFormType } from "types/common";
 
-const $q = useQuasar();
-
 definePageMeta({
   title: "팀 만들기",
   layout: "empty",
@@ -14,10 +12,10 @@ definePageMeta({
 const saveTeam = async (formData: TeamFormType) => {
   const { error } = await GroupApi.create(formData);
   if (error.value?.statusCode === 409) {
-    $q.notify({ type: "negative", message: "중복된 도메인이 존재합니다!" });
+    Notify.create({ type: "negative", message: "중복된 도메인이 존재합니다!" });
     return;
   }
-  $q.notify({ type: "success", message: "등록되었습니다." });
+  Notify.create({ type: "success", message: "등록되었습니다." });
   await new Promise((res) => setTimeout(res, 500)); //타이밍 이슈가 있나보다.
   await navigateTo(`/@${formData.domain}/post`, { replace: true });
 };
