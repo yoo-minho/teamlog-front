1e63111313c23d130373456969a8f43793e36f2e
유민호 <wingedcompany@gmail.com>
2023-08-19 01:20:07 +0900

    다 해버려

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,20 @@
 <script setup lang="ts">
+useHead({
+  title: "팀로그 title",
+  meta: [
+    { property: "description", content: `팀로그짱 description` },
+    { property: "og:title", content: "The Intuitive Web Framework" },
+    {
+      property: "og:description",
+      content:
+        "Build your next Vue.js application with confidence using Nuxt...",
+    },
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://nuxt.com" },
+    { property: "og:locale", content: "en_US" },
+    { property: "og:image", content: "https://nuxt.com/social.jpg" },
+  ],
+});
 definePageMeta({
   redirect: "/team",
 });
